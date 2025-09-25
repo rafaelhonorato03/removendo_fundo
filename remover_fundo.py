686d04e014bae7e81208bbd38c6d9d6309fbf235
Rafael Honorato <114627686+rafaelhonorato03@users.noreply.github.com>
2025-09-24 22:40:33 -0300

    rodagem de código

--- remover_fundo.py
+++ remover_fundo.py
@@ -1,7 +1,7 @@
 from rembg import remove
 from PIL import Image
 
-input_path = 'david_gilmour.jpg'  # Arqvuivo de entrada
+input_path = 'divulgacao quimica.png'  # Arqvuivo de entrada
 output_path = 'output.png'  # Arquivo para ser salvo
 
 inp = Image.open(input_path)  # Abrindo a imagem de entrada
